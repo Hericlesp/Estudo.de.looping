1a3d27b904ef25fd4e3ce46cd72d49cec08a0d68
Hericlesp <hericlespaulo05@gmail.com>
2024-10-15 21:58:39 -0300

    Create while.py

---
+++ while.py
@@ -0,0 +1,40 @@
+import os
+os.system('cls')
+num = 0
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+while True:
+    resp=str(input('deseja sair do looping? (s \ n)  ')).lower()
+    print("==" * 20)
+    if resp== "s":
+        import os
+        os.system('cls')
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        print("==" * 20)
+        print("   Numero é equivale a um total de : ", num)
+        print("==" * 20)
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        break
+    else:
+        import os
+        os.system('cls')
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        print("\n")
+        num +=1
+        print("==" * 20)
+        print("  Numero é equivale a um total de : ", num)
+        print("==" * 20)
